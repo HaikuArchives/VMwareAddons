d226baf0e33821f1e3218e53141778e0ead89a1a
Oscar Lesta <oscar.lesta@gmail.com>
2026-01-13 06:02:25 -0300

    shared_sources: code and style cleanups. No functional change intended.
    * Selectively applied haiku-format to the .cpp files.
      Left out the methods containing assembly, as-is, as haiku-format
      was making them look a bit worse, IMO.
      Left out the header files, for the same reason.
    * Manually updated style to match Haiku Coding Guidelines.
    * Removed default constructors/destructors.
    * Removed unused includes.
    * Used "fMemberName" naming style for class member variables.
    * Other minor clean ups, here and there.

--- shared_sources/VMWBackdoor.cpp
+++ shared_sources/VMWBackdoor.cpp
@@ -1,68 +1,63 @@
 /*
-	Copyright 2009 Vincent Duvert, vincent.duvert@free.fr
-	Copyright 2010-2011 Joshua Stein <jcs@jcs.org>
-	Copyright 2018 Gerasim Troeglazov <3dEyes@gmail.com>
-	All rights reserved. Distributed under the terms of the MIT License.
-*/
+ * Copyright 2009 Vincent Duvert, vincent.duvert@free.fr
+ * Copyright 2010-2011 Joshua Stein <jcs@jcs.org>
+ * Copyright 2018 Gerasim Troeglazov <3dEyes@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
 
 #include "VMWBackdoor.h"
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
-VMWBackdoor::VMWBackdoor()
-{
-}
-
-VMWBackdoor::~VMWBackdoor()
-{
-}
-
 status_t
 VMWBackdoor::EnableMouseSharing()
 {
-	if (!InVMware()) return B_NOT_ALLOWED;
-	
+	if (!InVMware())
+		return B_NOT_ALLOWED;
+
 	regs_t regs;
 	BackdoorCall(&regs, VMW_BACK_MOUSE_COMMAND, VMW_BACK_MOUSE_READ);
-	
+
 	// Check the status
-	BackdoorCall(&regs, VMW_BACK_MOUSE_STATUS, 0);
+	BackdoorCall(&regs, VMW_BACK_MOUSE_STATUS);
 	int16 status = HIGH_BITS(regs.eax);
 
 	if (status != 1)
 		return B_ERROR;
-	
+
 	// Check VMware's mouse driver version
 	BackdoorCall(&regs, VMW_BACK_MOUSE_DATA, 1);
 	if (regs.eax != VMW_BACK_MOUSE_VERSION)
 		return B_UNSUPPORTED;
-	
+
 	// We want absolute coordinates
 	BackdoorCall(&regs, VMW_BACK_MOUSE_COMMAND, VMW_BACK_MOUSE_REQ_ABSOLUTE);
 
 	return B_OK;
 }
 
+
 status_t
 VMWBackdoor::DisableMouseSharing()
 {
-	if (!InVMware()) return B_NOT_ALLOWED;
-	
+	if (!InVMware())
+		return B_NOT_ALLOWED;
+
 	regs_t regs;
 	BackdoorCall(&regs, VMW_BACK_MOUSE_COMMAND, VMW_BACK_MOUSE_DISABLE);
-	
+
 	// Check the status
-	BackdoorCall(&regs, VMW_BACK_MOUSE_STATUS, 0);
+	BackdoorCall(&regs, VMW_BACK_MOUSE_STATUS);
 	int16 status = HIGH_BITS(regs.eax);
 
 	if (status != -1)
 		return B_ERROR;
-	
+
 	return B_OK;
 }
+
 
 status_t
 VMWBackdoor::GetCursorPosition(int32& x, int32& y)
@@ -71,14 +66,14 @@ VMWBackdoor::GetCursorPosition(int32& x, int32& y)
 		return B_NOT_ALLOWED;
 
 	uint16 status;
-	uint16 to_read;
+	uint16 toRead;
 
-	GetCursorStatus(status, to_read);
+	GetCursorStatus(status, toRead);
 
 	if (status == VMWARE_ERROR)
 		return B_ERROR;
 
-	if (to_read == 0)
+	if (toRead == 0)
 		return B_INTERRUPTED;
 
 	regs_t regs;
@@ -91,17 +86,17 @@ VMWBackdoor::GetCursorPosition(int32& x, int32& y)
 
 
 void
-VMWBackdoor::GetCursorStatus(uint16& status, uint16& to_read)
+VMWBackdoor::GetCursorStatus(uint16& status, uint16& toRead)
 {
 	regs_t regs;
-	BackdoorCall(&regs, VMW_BACK_MOUSE_STATUS, 0);
+	BackdoorCall(&regs, VMW_BACK_MOUSE_STATUS);
 	status = HIGH_BITS(regs.eax);
-	to_read = LOW_BITS(regs.eax);
+	toRead = LOW_BITS(regs.eax);
 }
 
 
 status_t
-VMWBackdoor::GetHostClipboard(char** text, size_t* text_length)
+VMWBackdoor::GetHostClipboard(char** text, size_t* length)
 {
 	if (!InVMware())
 		return B_NOT_ALLOWED;
@@ -110,7 +105,7 @@ VMWBackdoor::GetHostClipboard(char** text, size_t* text_length)
 		return B_ERROR;
 
 	regs_t regs;
-	BackdoorCall(&regs, VMW_BACK_GET_CLIP_LENGTH, 0);
+	BackdoorCall(&regs, VMW_BACK_GET_CLIP_LENGTH);
 
 	uint32_t total, left;
 	total = left = regs.eax;
@@ -129,33 +124,35 @@ VMWBackdoor::GetHostClipboard(char** text, size_t* text_length)
 
 	for (;;) {
 		regs_t regs;
-		BackdoorCall(&regs, VMW_BACK_GET_CLIP_DATA, 0);
+		BackdoorCall(&regs, VMW_BACK_GET_CLIP_DATA);
 
 		memcpy(buffer + (total - left), &regs.eax, left > 4 ? 4 : left);
 
 		if (left <= 4) {
 			buffer[total] = '\0';
 			break;
-		} else
+		} else {
 			left -= 4;
+		}
 	}
 
 	*text = buffer;
-	*text_length = strlen(buffer);
+	*length = strlen(buffer);
 
 	return B_OK;
 }
 
+
 status_t
-VMWBackdoor::SetHostClipboard(char* text, size_t text_length)
+VMWBackdoor::SetHostClipboard(char* text, size_t length)
 {
 	if (!InVMware())
 		return B_NOT_ALLOWED;
 
-	if (text == NULL || text_length == 0)
+	if (text == NULL || length == 0)
 		return B_ERROR;
 
-	text[text_length] = '\0';
+	text[length] = '\0';
 
 	uint32_t total, left;
 	total = left = strlen(text);
@@ -189,7 +186,8 @@ VMWBackdoor::GetGUISetting(gui_setting setting)
 	return (regs.eax & setting) != 0;
 }
 
-/*
+
+#ifdef SET_GUI_SETTINGS
 void
 VMWBackdoor::SetGUISetting(gui_setting setting, bool enabled)
 {
@@ -209,7 +207,8 @@ VMWBackdoor::SetGUISetting(gui_setting setting, bool enabled)
 
 	BackdoorCall(&regs, VMW_BACK_SET_GUI_SETTING, setting);
 }
-*/
+#endif // SET_GUI_SETTINGS
+
 
 ulong
 VMWBackdoor::GetHostClock()
@@ -225,7 +224,7 @@ VMWBackdoor::GetHostClock()
 		return 0;
 
 	// EAX contains the GMT (in seconds since 1/1/1970)
-	// On WS4.0/GSX2.5 and earlier EDX contained the timezone offset (in minutes),
+	// On WS4.0/GSX2.5, and earlier, EDX contained the timezone offset (in minutes),
 	// but returns 0 on WS4.5/GSX3.2 and later.
 	return regs.eax;
 }
